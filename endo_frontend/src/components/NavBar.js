089a0e77c8ce48c9b40e40fd4975465793291f2c
Phạm Huy <104686007+HueyPixelBoxes@users.noreply.github.com>
2025-07-10 11:41:55 +0700

    Add  structure

---
+++ endo_frontend/src/components/NavBar.js
@@ -0,0 +1,18 @@
+//Navigation Bar to move between pages
+
+function NavBar(){
+    return (
+        <nav className="bg-sky-800 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-white text-5xl font-bold">EndoDetect</div>
+                <div className="space-x-4 text-xl">
+                <a href="/diagnostic" className="text-gray-300 hover:text-white">Diagnostic</a>
+                <a href="/analytic" className="text-gray-300 hover:text-white">Analytic</a>
+                <a href="/showcase" className="text-gray-300 hover:text-white">Showcase</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
